372eab584bae38f827489be43db8aa8192c749b7
Stepan_Prokopiak <Stepan_Prokopiak@epam.com>
2018-03-28 15:44:18 +0300

    Added practice with http requests

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,86 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters and mixins</h1>
-                <p>{{text | uppercase | to-lowercase}}</p>
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input class="form-control" type="text" v-model="user.username">
+                </div>
+                 <div class="form-group">
+                    <label for="">Mail</label>
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
-                <button @click="fruits.push('Berries')">Add</button>
-                <input placeholder="Search" v-model="filterText" type="text">
-                <ul>
-                    <li v-for="fruit in filteredFruits" :key="fruit">{{fruit}}</li>
+                <input class="form-control" type="text" v-model="node">
+                <br>
+                <br>
+                <button class="btn btn-primary" @click="fetchData">Get data</button>
+                <br>
+                <br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users" :key='u'>{{u.username}} - {{u.email}}</li>
                 </ul>
-                <hr>
-                <appList></appList>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import List from './List.vue';
-    import {fruitMixin} from './fruitMixit';
 
     export default {
         data() {
-            return {
-                text: 'Hello there!',
-                // fruits: ['Apple', 'Orange', 'Mango', 'Banana'],
-                // filterText: ''
+            return{
+                user: {
+                    username: '',
+                    email: ''
+                },
+                users: [],
+                resource: [],
+                node: 'data'
             }
         },
-        filters: {
-            uppercase(value){
-                return value.toUpperCase()
+        methods: {
+            submit(){
+                // this.$http.post('data.json', this.user)
+                //     .then(response =>{
+                //         console.log(response)
+                //     }, error =>{
+                //         console.log(error)
+                //     });
+
+                // this.resource.save({}, this.user)
+                this.resource.saveAlt(this.user)
+            },
+            fetchData(){
+                // this.$http.get('data.json')
+                //     .then(response =>{
+                //         return response.json();
+                //     })
+                //     .then(data=>{
+                //         for(var key in data){
+                //             this.users.push(data[key])
+                //         }
+                //     });
+                 this.resource.getData({node: this.node})
+                    .then(response =>{
+                        return response.json();
+                    })
+                    .then(data=>{
+                        for(var key in data){
+                            this.users.push(data[key])
+                        }
+                    });
             }
         },
-        // computed: {
-        //     filteredFruits(){
-        //         return this.fruits.filter((elem)=>{
-        //             return elem.match(this.filterText);
-        //         });
-        //     }
-        // },
-        components: {
-            appList: List
-        },
-        mixins: [fruitMixin]
+        created(){
+            const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
+            }
+            // this.resource = this.$resource('data.json')
+            this.resource = this.$resource('{node}.json', {}, customActions)
+        }
     }
 </script>
 
