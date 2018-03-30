32032ea89c9d5d9867f02040188c556457ad3463
Stepan_Prokopiak <Stepan_Prokopiak@epam.com>
2018-03-30 11:27:47 +0300

    Added new vuex practice

--- src/store/store.js
+++ src/store/store.js
@@ -1,54 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './module/counter';
+
+import * as actions from './actions';
+import * as mutations from './mutations';
+import * as getters from './getters'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value: 0
     }, 
-    getters:{
-        doubleCounter: state =>{
-            return state.counter *2;
-        },
-        stateCounter: state => {
-            return state.counter + ' Clicks';
-        },
-        value: state =>{
-            return state.value
-        }
-    },
-    mutations: {
-        increment: (state, payload) =>{
-            state.counter += payload;
-        },
-        decrement: state =>{
-            state.counter --;
-        },
-        updateValue: (state, payload) =>{
-            state.value = payload
-        }
-    },
-    actions: {
-        increment: ({commit}, payload) =>{
-            commit('increment', payload);
-        },
-        decrement: ({commit}) =>{
-            commit('decrement');
-        },
-        asyncIncrement: ({commit}, payload) =>{
-            setTimeout(()=>{
-                commit('increment', payload.by);
-            },payload.duration);
-        },
-        asyncDecrement: ({commit}) =>{
-            setTimeout(()=>{
-                commit('decrement');
-            },3000);
-        },
-        updateValue: ({commit}, payload) =>{
-            commit('updateValue', payload);
-        }
+    // getters:{
+    //     value: state =>{
+    //         return state.value
+    //     }
+    // },
+    // mutations: {
+    //     updateValue: (state, payload) =>{
+    //         state.value = payload
+    //     }
+    // },
+    // actions: {
+    //     updateValue: ({commit}, payload) =>{
+    //         commit('updateValue', payload);
+    //     }   
+    // },
+    actions,
+    mutations,
+    getters,
+    modules: {
+        counter
     }
 })
